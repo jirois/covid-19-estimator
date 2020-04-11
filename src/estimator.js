e34757cec454d0d5a011a1ca9bb45a9add78c9cc
Omas Ajiri <jicintech@gmail.com>
2020-04-11 02:24:22 +0100

    assessment for challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,16 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    let reportedCases = data.reportedCases
+    return{
+        impact:{
+            currentlyInfected : reportedCases * 10,
+            infectionsByRequestedTime: currentlyInfected * 512
+            
+        },
+        severeImpact:{
+            currentlyInfected: reportedCases * 50,
+            infectionsByRequestedTime: currentlyInfected * 512
+        }
+    }
+};
 
 export default covid19ImpactEstimator;
