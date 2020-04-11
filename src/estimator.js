e8536351d9a0dfc1b1dc58d9408d24b51bb736d0
Omas Ajiri <jicintech@gmail.com>
2020-04-11 02:43:25 +0100

    lint corrections

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,14 @@
 const covid19ImpactEstimator = (data) => {
-    let reportedCases = data.reportedCases
-    return{
+    const {reportedCases} = data;
+    return {
         impact:{
-            currentlyInfected : reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * 512
+          currentlyInfected : reportedCases * 10,
+          infectionsByRequestedTime: this.currentlyInfected * 512
             
         },
         severeImpact:{
-            currentlyInfected: reportedCases * 50,
-            infectionsByRequestedTime: currentlyInfected * 512
+          currentlyInfected: reportedCases * 50,
+          infectionsByRequestedTime: this.currentlyInfected * 512
         }
     }
 };
